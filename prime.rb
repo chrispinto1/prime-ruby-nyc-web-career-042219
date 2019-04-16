8418ee3f94f258ea5005256135ca938e016300c2
Christian Pinto <pintochristian2@gmail.com>
2019-04-16 00:28:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1 
+    return false
+  elsif num == 2
+    return true
+  else
+    #(2..num/2).none? { |i| num % i == 0}
+    prime = []
+    range = (2..num).to_a
+    range.each do |i|
+      puts num
+      if num % i == 0
+        prime.push(i)
+      end
+      puts prime
+      if prime[0] % 2 == 0   
+        puts false
+      else 
+        return true
+      end
+    end
+  end
+end
